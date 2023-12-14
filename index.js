27590e607f08a24dd3f9ddf367e7f3ab9e9081c2
salil tripathi <saliltripathi301@gmail.com>
2023-12-14 16:01:40 +0530

    commiting all files

---
+++ index.js
@@ -0,0 +1,13 @@
+// // let audio=new Audio("air.mp3")
+// // console.log(audio);
+// let turn="X";
+// const changeTurn=()=>{
+//     return turn==="X"?0:turn==="X";
+// }
+// const checkWin=()=>{
+//     return 
+// }
+
+// // Main logic of a program
+// const sound=document.getElementsByClassName("box")
+// console.log(sound);
